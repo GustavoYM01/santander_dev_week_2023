d469b1dd13e1d206cc6820c2149ce972dcb16aee
GustavoYM01 <gumatimoto@hotmail.com>
2024-07-05 09:45:52 -0300

    Dependência do Swagger adicionada, bugfix CORS, validações e perfil de prd

--- src/main/java/me/dio/santander_dev_week_2023/domain/repository/UserRepository.java
+++ src/main/java/me/dio/santander_dev_week_2023/domain/repository/UserRepository.java
@@ -7,4 +7,5 @@ import me.dio.santander_dev_week_2023.domain.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByAccountNumber(String accountNumber);
 }
